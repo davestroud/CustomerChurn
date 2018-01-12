22089fe42301b4cd73a495a68e771bf2bccb3a40
David Stroud <david@greymatter-analytics.com>
2018-01-12 13:58:14 -0600

    Add required libraries, to include keras, and used glimpse to see view observations and variables.

--- Main.R
+++ Main.R
@@ -1,3 +1,15 @@
+# Load libraries
 library(readr)
+library(keras)
+library(lime)
+library(tidyquant)
+library(rsample)
+library(recipes)
+library(yardstick)
+library(corrr)
+
+# Import IBM Churn Data
 churn_data_raw <- read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
-View(churn_data_raw)
+
+
+glimpse(churn_data_raw)
