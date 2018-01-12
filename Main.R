d33054e3269a04a19106e4c80c5c6adf5ac02246
David Stroud <david@greymatter-analytics.com>
2018-01-12 17:23:12 -0600

    Split into Train/Test sets

--- Main.R
+++ Main.R
@@ -23,5 +23,8 @@ churn_data_tbl <- churn_data_raw %>%
 # View observations and variables
 glimpse(churn_data_tbl)
 
-
+# Split test/training sets ~ uses the rsample package
+set.seed(100)
+train_test_split <- initial_split(churn_data_tbl, prop = 0.8)
+train_test_split
         
