2e879a72a26d2e13a1c0c6152e4827593302f7ac
David Stroud <david@greymatter-analytics.com>
2018-01-12 14:22:39 -0600

    Remove customer id, na and churn

--- Main.R
+++ Main.R
@@ -11,5 +11,17 @@ library(corrr)
 # Import IBM Churn Data
 churn_data_raw <- read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
-
+# View observations and variables
 glimpse(churn_data_raw)
+
+# Remove unnecessary data
+churn_data_tbl <- churn_data_raw %>%
+  select(-customerID) %>% # Remove Customer ID
+  drop_na() %>% # drop NA observations
+  select(Churn, everything()) # Remove all of Churn variable
+
+# View observations and variables
+glimpse(churn_data_tbl)
+
+
+        
